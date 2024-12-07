5c573df12ef549ff45065031e51dfc49dee4bcbf
z-ella <162042862+z-ella@users.noreply.github.com>
2024-12-07 01:00:55 +0100

    Create billing.js

---
+++ billing.js
@@ -0,0 +1,15 @@
+function billingFunction(){
+  
+    if (document.getElementById('same').checked){
+      var name=document.getElementById('shippingName').value;
+      var zip=document.getElementById('shippingZip').value;
+      
+      document.getElementById('billingName').value = name; 
+      document.getElementById('billingZip').value = zip;
+      
+    }else{
+      document.getElementById('billingName').value = '';
+      document.getElementById('billingZip').value =  '';
+    }
+  
+}
